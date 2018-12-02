160ad90d15e072a80ad329a905cc764927201dbf
Barry de Kleijn <kleijn.barry@gmail.com>
2018-12-02 22:35:01 +0100

    Add chevron left & right icons

--- src/components/icons.js
+++ src/components/icons.js
@@ -325,6 +325,52 @@ const featherIcons = [
     }
   },
   {
+    name: 'bui-chevron-left',
+    icon: {
+      tag: 'svg',
+      attrs: {
+        xmlns: 'http://www.w3.org/2000/svg',
+        viewBox: '0 0 24 24',
+        fill: 'none',
+        stroke: 'currentColor',
+        strokeWidth: 2,
+        strokeLinecap: 'round',
+        strokeLinejoin: 'round'
+      },
+      children: [
+        {
+          tag: 'polyline',
+          attrs: {
+            points: '15 18 9 12 15 6'
+          }
+        }
+      ]
+    }
+  },
+  {
+    name: 'bui-chevron-right',
+    icon: {
+      tag: 'svg',
+      attrs: {
+        xmlns: 'http://www.w3.org/2000/svg',
+        viewBox: '0 0 24 24',
+        fill: 'none',
+        stroke: 'currentColor',
+        strokeWidth: 2,
+        strokeLinecap: 'round',
+        strokeLinejoin: 'round'
+      },
+      children: [
+        {
+          tag: 'polyline',
+          attrs: {
+            points: '9 18 15 12 9 6'
+          }
+        }
+      ]
+    }
+  },
+  {
     name: 'bui-chevron-up',
     icon: {
       tag: 'svg',
