d2e1ed53042c733e0f42a2890b048a051cbd148f
Barry de Kleijn <kleijn.barry@gmail.com>
2019-01-02 23:51:03 +0100

    Updates

---
+++ precache-manifest.339eddd36b438fc3c42bdbc948269ef3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "75fcab41955599c81c74",
+    "url": "/bui/static/js/runtime~main.75fcab41.js"
+  },
+  {
+    "revision": "a09e06e8cdd49f404d38",
+    "url": "/bui/static/js/main.a09e06e8.chunk.js"
+  },
+  {
+    "revision": "16e56533901645b7c0f1",
+    "url": "/bui/static/js/1.16e56533.chunk.js"
+  },
+  {
+    "revision": "a09e06e8cdd49f404d38",
+    "url": "/bui/static/css/main.ab7f0122.chunk.css"
+  },
+  {
+    "revision": "ec0dcfa016b14564d46a2ba3be670aad",
+    "url": "/bui/index.html"
+  }
+];
